abd3397c617dd088e5c63e7fdd7dd380eea67b80
Bhargavi Vankayala <146963251+Bhargavi135@users.noreply.github.com>
2023-10-13 23:07:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function reading(){
+    console.log("bhargavi is reading");
+}
+
+function writing(){
+    console.log("bhargavi is writing");
+}
+
+function listening(){
+    console.log("bhargavi is listening");
+}
+
+
+reading();
+
+
+function playing(){
+    console.log("hello world playing");
+}
+
+playing();
+
+let digit1 = 33
+let digit2 = 55
+let digit3 = 44
+
+function addition(){
+    console.log(digit1+digit2);
+}
+
+addition();
+
+function subtraction(){
+    console.log(digit1-digit2);
+}
+
+subtraction();
+
+console.log("hello world")
+
+for(let i=0; i<10; i++){
+    console.log("hello world")
+}
